978144be550e7b3a7ed43fd0eb57949a0a922779
Kayla Kelly <76846236+Kaylawkelly@users.noreply.github.com>
2021-02-04 21:37:47 -0500

    Before You Begin Portion
    Adding files necessary for project

---
+++ PyBank/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 21:19:59 2021
+
+@author: kay
+"""
+
---
+++ PyPoll/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 21:29:00 2021
+
+@author: kay
+"""
+
