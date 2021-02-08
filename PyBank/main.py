5254b53d3b9babc51916eba76d0aa90069966ae5
Kayla Kelly <76846236+Kaylawkelly@users.noreply.github.com>
2021-02-08 18:08:52 -0500

    pybank / pypoll complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,4 +5,78 @@ Created on Thu Feb  4 21:19:59 2021
 
 @author: kay
 """
+import csv
+import os
 
+csvpath = os.path.join('Resources', 'budget_data.csv')
+pathout = os.path.join('Analysis','budget_analysis.txt')
+
+total_months = []
+revenue = []
+average_revenue_change = []
+monthly_change = []
+    
+
+
+
+with open(csvpath, newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    print(reader)
+    csvheader = next(reader)
+    
+    print(f"Header: {csvheader}")               
+
+#Total Months       
+    for row in reader:
+        total_months.append(row[0])
+        revenue.append(row[1])
+    print(len(total_months))
+#Total Revenue 
+    revenue_int = map(int,revenue)
+    total_revenue = (sum(revenue_int))
+    print(total_revenue)
+
+ #Average Monthly Change
+    i = 0
+    for i in range(len(revenue) - 1):
+        profit_loss = int(revenue[i+1]) - int(revenue[i])
+ 
+
+        average_revenue_change.append(profit_loss)
+    Total = sum(average_revenue_change)
+    #print(revenue_change)
+    monthly_change = Total / len(average_revenue_change)
+    print(monthly_change)
+    #print(Total)
+    
+#Greatest Increase in Profits
+    greatest_increase = max(average_revenue_change)
+    print(greatest_increase)
+    j = average_revenue_change.index(greatest_increase)
+    month_increase = total_months[j+1]
+    
+#Greatest Decrease in Profice
+    greatest_decrease = min(average_revenue_change)
+    print(greatest_decrease)
+    x = average_revenue_change.index(greatest_decrease)
+    month_decrease = total_months[x+1]
+
+
+print(f'Financial Analysis'+'\n')
+print(f'----------------------------'+'\n')
+print("Total number of months: " + str(len(total_months)))
+print("Total Revenue in period: $ " + str(total_revenue))   
+print("Average monthly change in Revenue : $" + str(monthly_change))
+print(f"Greatest Increase in Profits: {month_increase} (${greatest_increase})")
+print(f"Greatest Decrease in Profits: {month_decrease} (${greatest_decrease})")
+
+file = open(pathout,'w')
+
+
+file.write(f'Financial Analysis'+'\n')
+file.write(f'----------------------------'+'\n')
+file.write("Total number of months: " + str(len(total_months)))
+file.write("Total Revenue in period: $ " + str(total_revenue)) 
+file.write("Average monthly change in Revenue : $" + str(monthly_change))
+file.write(f"Greatest Increase in Profits: {month_increase} (${greatest_increase})")
+file.write(f"Greatest Decrease in Profits: {month_decrease} (${greatest_decrease})")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,4 +5,64 @@ Created on Thu Feb  4 21:29:00 2021
 
 @author: kay
 """
+import os
+import csv
 
+csvpath = os.path.join("Resources","election_data.csv")
+pathout = os.path.join("Analysis","election_analysis.txt")
+
+
+totalVotes = 0
+candidates = []
+votecount = []
+winnervotercount = 0
+
+
+with open(csvpath, 'r', newline='') as csvfile:        
+    
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+
+    for row in csvreader:
+        totalVotes += 1
+
+        if(row[2] not in candidates):
+            candidates.append(row[2])
+            votecount.append(0)
+        
+       
+        candidateIndex = candidates.index(row[2])
+        votecount[candidateIndex] += 1
+
+
+    print(f"\nElection Results\n-------------------------------------------")
+    print(f"Total votes: {totalVotes}")
+    print("-------------------------------------------")
+    
+    for x in range(len(candidates)):
+        votePercent = round((votecount[x]/totalVotes)*100,3)
+        print(f"{candidates[x]}: {votePercent}% ({votecount[x]})")
+        if (winnervotercount<votecount[x]):
+            winnervotercount = votecount[x]
+            winner = candidates[x]
+    
+    print("-------------------------------------------")
+    print(f"Winner: {winner}")
+    print("-------------------------------------------")
+
+file = open(pathout,'w')
+
+
+file.write("Election Anaylsis")
+file.write("\n-------------------------------------------")
+file.write("\nTotal votes:" + str(totalVotes))
+file.write("\n-------------------------------------------")
+    
+for x in range(len(candidates)):
+    votePercent = round((votecount[x]/totalVotes)*100,3)
+    file.write("\n" + str(candidates[x]) +" : " + str(votePercent) 
+                + "% ("+ str(votecount[x]) + ")")
+file.write("\n-------------------------------------------")
+file.write("\nWinner: " + str(winner))
+file.write("\n-------------------------------------------")
